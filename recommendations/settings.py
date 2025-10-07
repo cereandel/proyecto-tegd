c91097aa75e5155f8660cd0f39f089ca7f75e7ae
Cesar Reyes <cereandel@gmail.com>
2025-10-07 18:41:55 -0400

    initial connection with mongodb

--- recommendations/settings.py
+++ recommendations/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myfirstapp'
 ]
 
 MIDDLEWARE = [
@@ -72,12 +73,12 @@ WSGI_APPLICATION = 'recommendations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
